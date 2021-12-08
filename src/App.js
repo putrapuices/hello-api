f998e7c9d847ab4b5ebf834a93951597809a0dc0
putrapuices <puices14@gmail.com>
2021-12-08 21:33:39 +0700

    Interaksi Dengan API (GET) ( 110 )

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header"></header>
+//     </div>
+//   );
+// }
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import axios from "axios";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataApi: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts");
+    // .then((response) => response.json())
+    // .then((json) => console.log(json));
+
+    // menyipan datanya
+    // .then((response) => response.json())
+    // .then((res) => {
+    //   this.setState({ dataApi: res });
+    // });
+
+    //menggunkan axios
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      // console.log(res);
+      // console.log(res.data);
+      this.setState({
+        dataApi: res.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello Api</p>
+        {this.state.dataApi.map((dat, index) => {
+          return (
+            <div key={index}>
+              <p>{dat.title}</p>
+              <p>{dat.body}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
